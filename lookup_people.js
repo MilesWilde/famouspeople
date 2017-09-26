446b466d318c3839888894e5f4f11489a09e7201
MilesWilde <craigmilde_@hotmail.com>
2017-09-26 15:43:17 +0000

    finished

---
+++ lookup_people.js
@@ -0,0 +1,33 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+const userInput = process.argv[2];
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query(`SELECT * FROM famous_people WHERE first_name ILIKE '%${userInput}%' OR last_name ILIKE '%${userInput}%'`, (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    printRows(result);
+    client.end();
+  });
+  console.log("Searching...")
+});
+
+function printRows(dataObject){
+  console.log(`Found ${dataObject.rows.length} person(s) by the name ${userInput}`)
+  for(let i in dataObject.rows){
+    console.log(`${Number(i)+1} ${dataObject.rows[i].first_name} ${dataObject.rows[i].last_name}, born ${dataObject.rows[i].birthdate}`);
+  }
+}
